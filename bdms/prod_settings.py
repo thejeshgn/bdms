a9156b6c777e7b3200e3675fc8cfeefcfd604011
thejeshgn <i@thejeshgn.com>
2015-01-24 23:39:27 +0530

    added static file settings

--- bdms/prod_settings.py
+++ bdms/prod_settings.py
@@ -11,11 +11,11 @@ DATABASES['default'] =  dj_database_url.config()
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
+
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, "bdms","static"),
     os.path.join(PROJECT_ROOT, "django_admin_bootstrapped","static"),
@@ -26,6 +26,3 @@ STATICFILES_DIRS = (
 
 BASE_URL='http://buzzdatabase.herokuapp.com/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
